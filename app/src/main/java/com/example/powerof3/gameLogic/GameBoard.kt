86dac8e47b1560fe715c20241b81bcfbf262bc3b
Chavaldaniko <maksim.savchenko.05@gmail.com>
2025-11-30 16:34:02 +0700

    Created game functions

---
+++ app/src/main/java/com/example/powerof3/gameLogic/GameBoard.kt
@@ -0,0 +1,325 @@
+package com.example.powerof3.gameLogic
+import kotlin.random.Random
+
+class GameBoard(private val size: Int) {
+
+    private val board: Array<Array<Int>> = Array(size) { Array(size) {
+        0
+    } }
+
+    fun getSize() : Int {
+        return size
+    }
+
+    fun clear() {
+        for (row in 0..size-1) {
+            for (col in 0..size-1) {
+                board[row][col] = 0
+            }
+        }
+    }
+
+    fun getElementValue(row: Int, col: Int) : Int {
+        return board[row][col]
+    }
+
+    fun set(row: Int, col: Int, v: Int) {
+        board[row][col] = v
+    }
+
+    fun moveLeft() : Pair<Boolean, Int> {
+        var score = 0
+        var isMoved = false
+        for (i in 0..size-1) {
+            val (rowMoved, rowScore) = leftMoveRow(i)
+            isMoved = if (rowMoved) true else isMoved
+            score += rowScore
+        }
+
+        return Pair<Boolean, Int>(isMoved, score)
+    }
+
+    fun moveRight() : Pair<Boolean, Int> {
+        var score = 0
+        var isMoved = false
+        for (i in 0..size-1) {
+            val (rowMoved, rowScore) = rightMoveRow(i)
+            isMoved = if (rowMoved) true else isMoved
+            score += rowScore
+        }
+
+        return Pair<Boolean, Int>(isMoved, score)
+    }
+
+    fun moveUp() : Pair<Boolean, Int> {
+        var score = 0
+        var isMoved = false
+        for (i in 0..size-1) {
+            val (colMoved, colScore) = upMoveCol(i)
+            isMoved = if (colMoved) true else isMoved
+            score += colScore
+        }
+
+        return Pair<Boolean, Int>(isMoved, score)
+    }
+
+    fun moveDown() : Pair<Boolean, Int> {
+        var score = 0
+        var isMoved = false
+        for (i in 0..size-1) {
+            val (colMoved, colScore) = downMoveCol(i)
+            isMoved = if (colMoved) true else isMoved
+            score += colScore
+        }
+
+        return Pair<Boolean, Int>(isMoved, score)
+    }
+
+    fun addRandom() {
+
+        val emptyCells = mutableListOf<Pair<Int, Int>>()
+        for (row in 0..size-1) {
+            for (col in 0..size-1) {
+                if (board[row][col] == 0) {
+                    emptyCells.add(Pair<Int, Int>(row, col))
+                }
+            }
+        }
+
+        if (emptyCells.isEmpty()) {
+            return
+        }
+
+        val isOne = Random.nextInt(0, 3) <= 1;
+        val idx = Random.nextInt(0, emptyCells.size);
+
+
+        if (isOne) {
+            board[emptyCells[idx].first][emptyCells[idx].second] = 1
+        }
+        else {
+            board[emptyCells[idx].first][emptyCells[idx].second] = 3
+        }
+
+
+    }
+
+    fun isAnyMovePossible() : Boolean {
+
+        for (row in 0..size-1) {
+            for (col in 0..size-1) {
+                if (isCellMovable(row, col)) {
+                    return true
+                }
+            }
+        }
+
+        return false
+
+    }
+
+    private fun isCellMovable(row: Int, col: Int) : Boolean {
+        if (board[row][col] == 0) {
+            return true
+        }
+
+        if (row >= 2 &&
+            board[row][col] == board[row-1][col] &&
+            board[row][col] == board[row-2][col]) {
+            return true
+        }
+
+        if (row <= size - 3 &&
+            board[row][col] == board[row+1][col] &&
+            board[row][col] == board[row+2][col]) {
+            return true
+        }
+
+        if (col >= 2 &&
+            board[row][col] == board[row][col-1] &&
+            board[row][col] == board[row][col-2]) {
+            return true
+        }
+
+        if (col <= size - 3 &&
+            board[row][col] == board[row][col+1] &&
+            board[row][col] == board[row][col+2]) {
+            return true
+        }
+
+        return false
+
+    }
+
+    private fun leftMoveRow(row: Int) : Pair<Boolean, Int> {
+
+        var score = 0
+
+        val nonEmptyTiles = mutableListOf<Int>()
+
+        for (i in 0..size-1) {
+            if (board[row][i] != 0) {
+                nonEmptyTiles.add(board[row][i])
+            }
+        }
+
+
+        var curIdx = 0
+        var i = 0
+
+        while (i < nonEmptyTiles.size) {
+
+            if (i < nonEmptyTiles.size - 2 &&
+                nonEmptyTiles[i] == nonEmptyTiles[i+1] &&
+                nonEmptyTiles[i+1] == nonEmptyTiles[i+2]) {
+
+                board[row][curIdx] = nonEmptyTiles[i] * 3
+                i += 3
+                score += board[row][curIdx]
+                curIdx++
+            }
+
+            else {
+                board[row][curIdx] = nonEmptyTiles[i]
+                i++
+                curIdx++
+            }
+
+        }
+
+        for (i in curIdx..size - 1) {
+            board[row][i] = 0
+        }
+        return Pair<Boolean, Int>(curIdx != size, score)
+    }
+
+    private fun rightMoveRow(row: Int) : Pair<Boolean, Int> {
+
+        var score = 0
+
+        val nonEmptyTiles = mutableListOf<Int>()
+
+        for (i in 0..size-1) {
+            if (board[row][i] != 0) {
+                nonEmptyTiles.add(board[row][i])
+            }
+        }
+
+
+        var curIdx = size - 1
+        var i = nonEmptyTiles.size - 1
+
+        while (i >= 0) {
+
+            if (i >= 2 &&
+                nonEmptyTiles[i] == nonEmptyTiles[i-1] &&
+                nonEmptyTiles[i-1] == nonEmptyTiles[i-2]) {
+
+                board[row][curIdx] = nonEmptyTiles[i] * 3
+                i -= 3
+                score += board[row][curIdx]
+                curIdx--
+            }
+
+            else {
+                board[row][curIdx] = nonEmptyTiles[i]
+                i--
+                curIdx--
+            }
+
+        }
+
+        for (i in 0..curIdx) {
+            board[row][i] = 0
+        }
+        return Pair<Boolean, Int>(curIdx != -1, score)
+    }
+
+    private fun upMoveCol(col: Int) : Pair<Boolean, Int> {
+
+        var score = 0
+
+        val nonEmptyTiles = mutableListOf<Int>()
+
+        for (i in 0..size-1) {
+            if (board[i][col] != 0) {
+                nonEmptyTiles.add(board[i][col])
+            }
+        }
+
+
+        var curIdx = 0
+        var i = 0
+
+        while (i < nonEmptyTiles.size) {
+
+            if (i < nonEmptyTiles.size - 2 &&
+                nonEmptyTiles[i] == nonEmptyTiles[i+1] &&
+                nonEmptyTiles[i+1] == nonEmptyTiles[i+2]) {
+
+                board[curIdx][col] = nonEmptyTiles[i] * 3
+                i += 3
+                score += board[curIdx][col]
+                curIdx++
+            }
+
+            else {
+                board[curIdx][col] = nonEmptyTiles[i]
+                i++
+                curIdx++
+            }
+
+        }
+
+        for (i in curIdx..size - 1) {
+            board[i][col] = 0
+        }
+        return Pair<Boolean, Int>(curIdx != size, score)
+    }
+
+    private fun downMoveCol(col: Int) : Pair<Boolean, Int> {
+
+        var score = 0
+
+        val nonEmptyTiles = mutableListOf<Int>()
+
+        for (i in 0..size-1) {
+            if (board[i][col] != 0) {
+                nonEmptyTiles.add(board[i][col])
+            }
+        }
+
+
+        var curIdx = size - 1
+        var i = nonEmptyTiles.size - 1
+
+        while (i >= 0) {
+
+            if (i >= 2 &&
+                nonEmptyTiles[i] == nonEmptyTiles[i-1] &&
+                nonEmptyTiles[i-1] == nonEmptyTiles[i-2]) {
+
+                board[curIdx][col] = nonEmptyTiles[i] * 3
+                i -= 3
+                score += board[curIdx][col]
+                curIdx--
+            }
+
+            else {
+                board[curIdx][col] = nonEmptyTiles[i]
+                i--
+                curIdx--
+            }
+
+        }
+
+        for (i in 0..curIdx) {
+            board[i][col] = 0
+        }
+        return Pair<Boolean, Int>(curIdx != -1, score)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/powerof3/gameLogic/GameClass.kt
@@ -0,0 +1,92 @@
+package com.example.powerof3.gameLogic
+
+import androidx.compose.ui.text.Paragraph
+
+enum class Moves {UP, DOWN, LEFT, RIGHT}
+
+
+class GameClass(private val boardSize: Int) {
+
+    private val board = GameBoard(boardSize)
+    private var scores = 0
+    private var gameOver = false
+
+    fun startGame() {
+        scores = 0
+        board.clear()
+        board.addRandom()
+        board.addRandom()
+        board.addRandom()
+    }
+
+    fun makeMove(move: Moves) {
+        val res = when(move) {
+            Moves.UP -> board.moveUp()
+            Moves.DOWN -> board.moveDown()
+            Moves.RIGHT -> board.moveRight()
+            Moves.LEFT -> board.moveLeft()
+        }
+
+        val (moved, score) = res
+        scores += score
+        if (!moved) {
+            gameOver = !board.isAnyMovePossible()
+        }
+        else {
+            board.addRandom()
+        }
+    }
+
+    fun isGameOver() : Boolean {
+        return gameOver
+    }
+
+    fun getScores() : Int {
+        return scores
+    }
+    fun getBoardSize() : Int {
+        return boardSize
+    }
+
+    fun getCellValue(row: Int, col: Int) : Int {
+
+        return board.getElementValue(row, col)
+
+    }
+
+}
+
+fun printGame(gameClass: GameClass) {
+
+    println("Scores : ${gameClass.getScores()}")
+    for (row in 0..gameClass.getBoardSize() - 1) {
+        for (col in 0..gameClass.getBoardSize() - 1) {
+            print(String.format("% 10d", gameClass.getCellValue(row, col)))
+        }
+        println()
+    }
+    println()
+}
+
+fun main() {
+
+    val g = GameClass(2)
+    g.startGame()
+    while (!g.isGameOver()) {
+
+        printGame(g)
+        print("Next move(WASD): ")
+        val move = readln()
+        when (move) {
+            "w" -> g.makeMove(Moves.UP)
+            "a" -> g.makeMove(Moves.LEFT)
+            "s" -> g.makeMove(Moves.DOWN)
+            "d" -> g.makeMove(Moves.RIGHT)
+            "e" -> break
+        }
+
+    }
+    print("GameOver");
+
+
+}
